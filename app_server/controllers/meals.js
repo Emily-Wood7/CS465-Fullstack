95e0f644295d754a82a835f20b7f4cf178f11cfc
erchilly <erchilly@gmail.com>
2023-07-10 20:06:46 -0400

    Convert rooms and meals template from static data to JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+
+var foods = JSON.parse(fs.readFileSync('./data/foods.json', 'utf8'));
+
 const meals = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Meals';
-    res.render('meals', {title: pageTitle});
+    res.render('meals', {title: pageTitle, foods});
 };
 
 module.exports = {
