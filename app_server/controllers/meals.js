8a98d2a2d25ffe0435bae7467bfe4f9ac39b02c6
erchilly <erchilly@gmail.com>
2023-07-24 18:18:15 -0400

    Modify Public Website to Use API Endpoints for Rooms and Meals

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,12 +1,63 @@
-var fs = require('fs');
+//var fs = require('fs');
+//var foods = JSON.parse(fs.readFileSync('./data/foods.json', 'utf8'));
 
-var foods = JSON.parse(fs.readFileSync('./data/foods.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-const meals = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Meals';
-    res.render('meals', {title: pageTitle, foods});
+// const meals = (req, res) => {
+//     pageTitle = process.env.npm_package_description + ' - meals';
+//     res.render('meals', {title: pageTitle, foods});
+// };
+
+// module.exports = {
+//     meals
+// };
+
+// render the meals list view
+const renderMealsList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Meals';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No foods exist in our database!';
+        }
+    }
+    res.render('meals',
+        {
+            title: pageTitle,
+            foods: responseBody,
+            message
+        }
+    );
+}
+
+// GET meals list view
+const mealsList = (req, res) => {
+    const path = '/api/foods';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> mealsController.mealsList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderMealsList(req, res, body);
+        }
+    );
 };
 
 module.exports = {
-    meals
+    mealsList
 };
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,12 +1,63 @@
-var fs = require('fs');
+//var fs = require('fs');
+//var rooms = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
 
-var room = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-const rooms = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Rooms';
-    res.render('rooms', {title: pageTitle, room});
+// const rooms = (req, res) => {
+//     pageTitle = process.env.npm_package_description + ' - rooms';
+//     res.render('rooms', {title: pageTitle, rooms});
+// };
+
+// module.exports = {
+//     rooms
+// };
+
+// render the rooms list view
+const renderRoomsList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Rooms';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No rooms exist in our database!';
+        }
+    }
+    res.render('rooms',
+        {
+            title: pageTitle,
+            room: responseBody,
+            message
+        }
+    );
+}
+
+// GET rooms list view
+const roomsList = (req, res) => {
+    const path = '/api/rooms';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> roomsController.roomsList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderRoomsList(req, res, body);
+        }
+    );
 };
 
 module.exports = {
-    rooms
+    roomsList
 };
