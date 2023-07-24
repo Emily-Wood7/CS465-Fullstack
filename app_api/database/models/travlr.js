bcab9ae889b889f57ec2108564dd27277777298f
erchilly <erchilly@gmail.com>
2023-07-24 14:25:17 -0400

    Create rooms and foods object schemas with Mongoose, populate MongoDB

--- app_api/database/models/travlr.js
+++ app_api/database/models/travlr.js
@@ -12,5 +12,25 @@ const tripSchema = new mongoose.Schema({
     description: { type: String, required: true }
 });
 //mongoose.model('trips', tripSchema);
-
 module.exports = mongoose.model("trips", tripSchema);
+
+// define the room schema
+const roomSchema = new mongoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: { type: String, required: true, index: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    rate: { type: String, required: true }
+});
+
+module.exports = mongoose.model("rooms", roomSchema);
+
+// define the food schema
+const foodSchema = new mongoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: { type: String, required: true, index: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+module.exports = mongoose.model("foods", foodSchema);
